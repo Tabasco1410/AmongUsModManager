5aadd0e77dde9bd51cd4c974723beef4d81234d6
Tabasco1410 <traine233.yk@gmail.com>
2025-06-10 18:09:41 +0900

    Version表記の修正

--- Among Us_ModManeger/Updates/AppUpdate.cs
+++ Among Us_ModManeger/Updates/AppUpdate.cs
@@ -8,8 +8,8 @@ namespace Among_Us_ModManeger
 {
     public static class AppVersion
     {
-        public const string Version = "Version1.0";
-        public const string ReleaseDate = "2025-06-09";
-        public const string Notes = "初期バージョンです。";
+        public const string Version = "Version1.1";
+        public const string ReleaseDate = "2025-06-10";
+        public const string Notes = "UIの修正と、一部画面で戻るボタンが機能していなかった問題の修正を行いました。";
     }
 }
--- Among Us_ModManeger/obj/Release/net8.0-windows/win-x64/Among Us_ModManeger.AssemblyInfo.cs
+++ Among Us_ModManeger/obj/Release/net8.0-windows/win-x64/Among Us_ModManeger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Among Us_ModManeger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872bfa7fa9f8bcd63c8899207e86201877dd4b97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063fa1b785a5db851e865b589f6cd550aa8cba4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Among Us_ModManeger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Among Us_ModManeger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
